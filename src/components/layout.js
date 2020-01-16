7c160524c71f4ef041e4e179928fd281e7365a6c
Robert Agthe <robert@scriptshit.de>
2020-01-16 19:08:19 +0100

    added hero image

--- src/components/layout.js
+++ src/components/layout.js
@@ -30,6 +30,11 @@ const TemplateWrapper = ({ children }) => {
               }
             }
             copyright
+            background {
+              fixed(width: 1200) {
+                src
+              }
+            }
           }
           allDatoCmsSocialProfile(sort: { fields: [position], order: ASC }) {
             edges {
@@ -42,70 +47,72 @@ const TemplateWrapper = ({ children }) => {
         }
       `}
       render={data => (
-        <div className={`container ${showMenu ? 'is-open' : ''}`}>
-          <HelmetDatoCms
-            favicon={data.datoCmsSite.faviconMetaTags}
-            seo={data.datoCmsHome.seoMetaTags}
-          />
-          <div className='container__sidebar'>
-            <div className='sidebar'>
-              <h6 className='sidebar__title'>
-                <Link to='/'>{data.datoCmsSite.globalSeo.siteName}</Link>
-              </h6>
-              <div
-                className='sidebar__intro'
-                dangerouslySetInnerHTML={{
-                  __html:
-                    data.datoCmsHome.introTextNode.childMarkdownRemark.html
-                }}
-              />
-              <ul className='sidebar__menu'>
-                <li>
-                  <Link to='/'>Home</Link>
-                </li>
-                <li>
-                  <Link to='/about'>About</Link>
-                </li>
-              </ul>
-              <p className='sidebar__social'>
-                {data.allDatoCmsSocialProfile.edges.map(({ node: profile }) => (
-                  <a
-                    key={profile.profileType}
-                    href={profile.url}
-                    target='blank'
-                    className={`social social--${profile.profileType.toLowerCase()}`}
-                  >
-                    {' '}
-                  </a>
-                ))}
-              </p>
-              <div className='sidebar__copyright'>
-                {data.datoCmsHome.copyright}
-              </div>
-            </div>
-          </div>
-          <div className='container__body'>
-            <div className='container__mobile-header'>
-              <div className='mobile-header'>
-                <div className='mobile-header__menu'>
-                  <a
-                    href='/'
-                    onClick={e => {
-                      e.preventDefault()
-                      setShowMenu(!showMenu)
-                    }}
-                  >&nbsp;
-                  </a>
-                </div>
-                <div className='mobile-header__logo'>
+        <>
+          <div className='hero' style={{ backgroundImage: `url(${data.datoCmsHome.background.fixed.src})` }} />
+          <div className={`container ${showMenu ? 'is-open' : ''}`}>
+            <HelmetDatoCms
+              favicon={data.datoCmsSite.faviconMetaTags}
+              seo={data.datoCmsHome.seoMetaTags}
+            />
+            <div className='container__sidebar'>
+              <div className='sidebar'>
+                <h6 className='sidebar__title'>
                   <Link to='/'>{data.datoCmsSite.globalSeo.siteName}</Link>
+                </h6>
+                <div
+                  className='sidebar__intro'
+                  dangerouslySetInnerHTML={{
+                    __html:
+                    data.datoCmsHome.introTextNode.childMarkdownRemark.html
+                  }}
+                />
+                <ul className='sidebar__menu'>
+                  <li>
+                    <Link to='/'>Home</Link>
+                  </li>
+                  <li>
+                    <Link to='/about'>About</Link>
+                  </li>
+                </ul>
+                <p className='sidebar__social'>
+                  {data.allDatoCmsSocialProfile.edges.map(({ node: profile }) => (
+                    <a
+                      key={profile.profileType}
+                      href={profile.url}
+                      target='blank'
+                      className={`social social--${profile.profileType.toLowerCase()}`}
+                    >
+                      {' '}
+                    </a>
+                  ))}
+                </p>
+                <div className='sidebar__copyright'>
+                  {data.datoCmsHome.copyright}
                 </div>
               </div>
             </div>
-            {children}
+            <div className='container__body'>
+              <div className='container__mobile-header'>
+                <div className='mobile-header'>
+                  <div className='mobile-header__menu'>
+                    <a
+                      href='/'
+                      onClick={e => {
+                        e.preventDefault()
+                        setShowMenu(!showMenu)
+                      }}
+                    >&nbsp;
+                    </a>
+                  </div>
+                  <div className='mobile-header__logo'>
+                    <Link to='/'>{data.datoCmsSite.globalSeo.siteName}</Link>
+                  </div>
+                </div>
+              </div>
+              {children}
+            </div>
           </div>
-        </div>
-      )}
+        </>)}
     />
   )
 }
